7573223eaacd855d264a6e1ab05668e4bd159f57
Anca Simon <ancarsimon@gmail.com>
2020-06-09 23:23:08 -0500

    added authentication via google signin in firebase and the seed data

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Travel Blog
 
 ## Description
-This project is an opportunity to practice all the skills we have learned with React - setting up a React project; completing CRUD actions with React and a Firebase database; authenticating with React and Firebase Google sign-in; and deploying a React project with Firebase. 
+This project is an opportunity to practice all the skills we have learned with React - setting up a React project; completing CRUD actions with React and a Firebase database; authenticating using React, Firebase, and Google sign-in; and deploying a React project with Firebase. 
 
 ## Background
 Project instructions can be found here: https://github.com/nss-nightclass-projects/exercise-vault/blob/master/REACT-travel_blog.md.
--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../pages/Auth/Auth';
+import MyNavbar from '../shared/MyNavbar/MyNavbar';
+
 import './App.scss';
 
+import fbConnection from '../helpers/data/connection';
+
+fbConnection();
+
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
     return (
       <div className="App">
-        <button class="btn btn-primary"><i class="fas fa-mountain"></i></button>
+        <MyNavbar />
+        <Auth />
       </div>
     );
   }
